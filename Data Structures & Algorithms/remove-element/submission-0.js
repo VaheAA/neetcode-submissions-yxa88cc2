df47a3629795b9b4584005ca4f5e90d259f8e674
Vahe Abovyan <vahe.abov@gmail.com>
2026-03-31 20:22:48 +0400

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+
+
+        let left = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            
+            if (nums[i] !== val) {
+                nums[left] = nums[i]
+                left++
+            } 
+        }
+        
+        return left
+    }
+}
