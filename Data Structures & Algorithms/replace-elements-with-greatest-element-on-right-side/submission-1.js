994f3bbd6eea42427d2ed0c91ca2b0dae8497873
Vahe Abovyan <vahe.abov@gmail.com>
2026-03-31 21:06:19 +0400

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+
+        let maxSoFar = -1
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+
+            const original = arr[i]
+            arr[i] = maxSoFar
+            maxSoFar = Math.max(maxSoFar, original)
+        }
+
+
+
+        return arr
+    }
+}
