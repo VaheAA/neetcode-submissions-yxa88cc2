ee61a2e01e3cea88fc150da04b7ca0a22222126f
Vahe Abovyan <vahe.abov@gmail.com>
2026-04-01 15:33:20 +0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.js
@@ -0,0 +1,45 @@
+class MinStack {
+    stack = []
+    minimals = []
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack[this.stack.length] = val
+        
+        if (this.minimals.length === 0) {
+            this.minimals[0] = val
+        }
+
+        else if (val <= this.minimals[this.minimals.length - 1]) {
+            this.minimals[this.minimals.length] = val
+        } 
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if (this.minimals[this.minimals.length - 1] === this.stack[this.stack.length - 1]) {
+            this.minimals.length = this.minimals.length - 1
+        }
+        this.stack.length = this.stack.length - 1
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minimals[this.minimals.length - 1]
+    }
+}
