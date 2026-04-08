57e0d16772c778f98ba2fee9afde68a724f33533
Vahe Abovyan <vahe.abov@gmail.com>
2026-04-08 11:29:14 +0400

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    anagramMappings(nums1, nums2) {
+        const mapping = []
+
+        let i = 0
+        let j = 0
+
+        while (i < nums1.length) {
+
+            if (nums1[i] === nums2[j]) {
+                mapping.push(j)
+
+                i++
+                
+                if (j === nums2.length - 1) j = 0
+                else j++
+            } else {
+              if (j === nums2.length - 1) j = 0
+                else j++
+            }
+
+               
+               
+        }
+
+        return mapping
+    }
+}
