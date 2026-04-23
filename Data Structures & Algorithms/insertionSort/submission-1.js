f1309cad316682270c7181f2145a0f082512c6a0
Vahe Abovyan <vahe.abov@gmail.com>
2026-04-23 15:44:49 +0400

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.js
@@ -0,0 +1,43 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs) {
+
+        if (!pairs.length) return []
+
+        let states = []
+
+
+        states.push(pairs.map(p => ({key: p.key, value: p.value})))
+        for (let i = 1; i < pairs.length; i++) {
+            let j = i - 1
+
+            while (j >= 0 && pairs[j+1]['key'] < pairs[j]['key']) {
+                        let tmp = pairs[j + 1];
+                        pairs[j + 1] = pairs[j];
+                        pairs[j] = tmp;
+                        j--;
+
+            }
+            states.push(pairs.map(p => ({key: p.key, value: p.value})))
+        }
+
+
+        return states
+    }
+}
