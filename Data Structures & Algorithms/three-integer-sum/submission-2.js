0ee95eabb99720c7b6a7ec660b04cc1730c74445
Vahe Abovyan <vahe.abov@gmail.com>
2026-06-25 16:07:34 +0400

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const sorted = nums.sort((a, b) => a - b)
+        const res = []
+
+        for (let i = 0; i < sorted.length; i++) {
+            if (i > 0 && sorted[i] === sorted[i-1]) continue
+
+            let left = i + 1
+            let right = sorted.length - 1
+
+            while (left < right) {
+                const sum = sorted[i] + sorted[left] + sorted[right]
+                if (sum === 0) {
+                    res.push([sorted[i], sorted[left], sorted[right]])
+                    right--
+                    left++
+
+                    while (left < right && sorted[left] === sorted[left - 1]) left++
+                    while (left < right && sorted[right] === sorted[right + 1]) right--
+                } else if (sum > 0) {
+                    right--
+                } else {
+                    left++
+                }
+            }
+        }
+
+        return res
+    }
+}
