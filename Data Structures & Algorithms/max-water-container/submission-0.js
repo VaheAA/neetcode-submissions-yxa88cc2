d113e080748443f51654d4fff68755f5927bef9c
Vahe Abovyan <vahe.abov@gmail.com>
2026-06-25 16:37:01 +0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+
+        let left = 0
+        let right = heights.length - 1
+        let max = 0
+
+        while (left < right) {
+            const width = right - left
+            const height = Math.min(heights[left],  heights[right])
+            const area = height * width
+            
+            if (area > max) {
+                max = area
+
+            }
+
+            if (heights[left] < heights[right]) {
+                 left++
+            } else {
+                 right--
+            }
+            
+        }
+
+
+        return max
+        
+    }
+}
