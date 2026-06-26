0079e42be70536cdfb4384e6def1c52319324977
Vahe Abovyan <vahe.abov@gmail.com>
2026-06-26 14:48:56 +0400

    Add: linked-list-cycle-detection - submission-9

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-9.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+
+        if (head === null || head.next == null) return false
+
+        let slow = head
+        let fast = head
+
+
+        while (fast !== null) {
+
+            if (fast.next == null || fast.next.next == null) return false
+
+            if (slow && slow.next)  slow = slow.next
+            fast = fast.next.next
+           
+            if (slow === fast) return true
+        }
+
+        return false
+    }
+}
